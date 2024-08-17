73a3e161bfd21d0524cbc1a02d739d01b32b8033
Mauricio Cox <coxmau77@gmail.com>
2024-08-17 16:40:58 -0300

    chore: utilizacion del transpilador de typescript al directorio dist

--- src/09-union-types.ts
+++ src/09-union-types.ts
@@ -1,12 +1,12 @@
-(()=>{
-  let userId: string  | Number;
+(() => {
+  let userId: string | Number;
   userId = 2024;
   userId = "año 20224";
 
   function greeting(content: string | number) {
     console.log(`txt: ${content}`);
     // guardians ts
-    if(typeof content === "string") {
+    if (typeof content === "string") {
       console.log(`string: ${content.toUpperCase()}`);
     } else {
       console.log(`number: ${content} metodo toFixed(1): ${content.toFixed(1)}`);
@@ -14,15 +14,15 @@
   }
 
   function defineType(content: string | number) {
-    typeof content === "number" ? content.toUpperCase() : content.toFixed(1)
+    typeof content === "number" ? content.toFixed(1) : content.toUpperCase()
   }
 
   greeting("Saludando desde una function");
   greeting(299.99);
 
   console.log(`Definir el tipo con una funcion:
-      string: ${defineType("Hola mundo!")}: type: ${typeof(defineType("Hola mundo!"))}
-      number: ${defineType(1999.99)}: type: ${typeof(defineType(1999.99))}
+      string: ${defineType("Hola mundo!")}: type: ${typeof (defineType("Hola mundo!"))}
+      number: ${defineType(1999.99)}: type: ${typeof (defineType(1999.99))}
     `);
 
 })();
